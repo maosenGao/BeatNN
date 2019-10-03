b677f1db1f7766db6044090735c24673958c6a57
iranroman <iran@ccrma.stanford.edu>
2019-10-03 09:46:06 -0700

    data preprocessing pipeline completed

--- experiments/lenet.py
+++ experiments/lenet.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mne
 
-data_dir = '../data/OpenMIIR/data/'
+data_dir = '../datasets/OpenMIIR/data/'
 file_name = 'P01-raw.fif'  
 
 # load the data
@@ -41,8 +41,6 @@ other trigger codes:
 ### get the data array and time array ###
 data, stim, time = raw.get_data()[:-1,:], raw.get_data()[-1,:], raw.times
 
-print(type(data[0,0]))
-
 ### find stim triggers for all C1 and C2 data ###
 triggers = (
 	11,  21,  31,  41,
@@ -65,22 +63,42 @@ nsecs = 1
 epoch_size = fs*nsecs
 nchans = 68
 
-X = np.empty([0,nchans,epoch_size])# dataset
-for t in range(3):#epoch_size):
+X = np.empty([0,nchans,epoch_size]) # dataset
+Y = np.empty([0,1]) #labels
+for t in range(epoch_size):
+	print('current t: ', t)
 	idx = np.linspace((trig_is-t),(trig_is-t)+epoch_size,num=epoch_size,dtype=int).T
 	X = np.append(
 		X,
 		np.transpose(data[:,idx],(1,0,2)),
 		axis=0)
+	Y = np.append(
+		Y,
+		np.ones((120,1),dtype=int)*t,
+		axis=0)
 
-base_secs = 0.1 # baseline
+base_secs = 0.1 # baseline period
 base_samp = np.floor(0.1*fs).astype(int)
 X = X - np.mean(X[:,:,:base_samp],axis=2,keepdims=True)
 
-plt.plot(X[0].T)
-plt.savefig('test.png')
+# separate into traning, validation, and test sets
+perc_tr = np.floor(0.85*X.shape[0]).astype(int) 
+perc_vl = np.floor(0.1*X.shape[0]).astype(int)
 
+rand_is = np.random.choice(X.shape[0],X.shape[0],replace=False)
+X = X[rand_is] # shuffle data
+Y = Y[rand_is] # shuffle data
 
+x_tr = X[:perc_tr]
+y_tr = Y[:perc_tr]
+x_vl = X[perc_tr:(perc_tr+perc_vl)]
+y_vl = Y[perc_tr:(perc_tr+perc_vl)]
+x_ts = X[(perc_tr+perc_vl):]
+y_ts = Y[(perc_tr+perc_vl):]
 
-
-
+np.save('tr_data/x_tr',x_tr)
+np.save('tr_data/y_tr',y_tr)
+np.save('tr_data/x_vl',x_vl)
+np.save('tr_data/y_vl',y_vl)
+np.save('ts_data/x_ts',x_ts)
+np.save('ts_data/y_ts',y_ts)
