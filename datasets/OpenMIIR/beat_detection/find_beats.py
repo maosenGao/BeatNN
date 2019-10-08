76d7607a16ba7f6f3afd9e830c45b6e8ccea01f2
iranroman <iran@ccrma.stanford.edu>
2019-10-08 12:23:11 -0700

    updates stim beat tracker and journal

--- datasets/OpenMIIR/beat_detection/find_beats.py
+++ datasets/OpenMIIR/beat_detection/find_beats.py
@@ -1,13 +1,24 @@
 import librosa
 import glob
+import numpy as np
 
 fs = 44100
-wav_directory = "../openmiir/audio/full.v2/"
-wav_filenames = glob.glob(wav_directory+"*.wav")
+wav_full_directory = "../openmiir/audio/full.v2/"
+wav_cues_directory = "../openmiir/audio/cues.v2/"
+wav_full_filenames = glob.glob(wav_full_directory+"*.wav")
+wav_cues_filenames = glob.glob(wav_cues_directory+"*.wav")
 
-for iwav in wav_filenames:
+
+for wav_file in wav_full_filenames:
 	
-	curr_wav, fs = librosa.load(iwav,fs)
-	tempo, beat_times = librosa.beat.beat_track(curr_wav, fs, units='time')
-	print(iwav, tempo, beat_times)
+	cue_file = wav_file[:18]+"cues"+wav_file[22:-4]+"_cue."+wav_file[-3:]
+
+	curr_stm, fs = librosa.load(wav_file,fs)
+	curr_cue, fs = librosa.load(cue_file,fs)
+
+	tempo, cue_beat_samps = librosa.beat.beat_track(curr_cue, fs, units='samples')
+	tempo, stm_beat_samps = librosa.beat.beat_track(curr_stm[len(curr_cue):], fs, units='samples')
+	print(np.diff(cue_beat_samps))
+	print(np.diff(stm_beat_samps))
+	#print(iwav, tempo, beat_times)
 	input()
