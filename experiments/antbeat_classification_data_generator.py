108ab5edaf325ac8741246edc572d4d7f5a5b226
iranroman <iran@ccrma.stanford.edu>
2019-11-05 19:03:48 -0800

    updtes jornal and data generation script

--- experiments/antbeat_classification_data_generator.py
+++ experiments/antbeat_classification_data_generator.py
@@ -10,7 +10,7 @@ subj_names_ids = antbeat_utils.data_utils.get_subj_names_ids_dict()
 phase_trigger_dict = antbeat_utils.data_utils.get_phases_trig_dict()
 trial_type_trigger_dict = antbeat_utils.data_utils.get_trial_type_trig_dict()
 phase_trial_type_trigger_dict = antbeat_utils.data_utils.generate_phase_trial_type_trigger_dict()	
-epoch_size = 76
+epoch_size = 75
 nchans = 64
 nlabels = 4
 
@@ -18,8 +18,10 @@ nlabels = 4
 # preparing the general montage
 montage = antbeat_utils.data_utils.get_eeg_montage(data_dir)
 
-all_subj_epochs = np.empty([0,nchans,epoch_size])
-all_subj_labels = np.empty([0,nlabels])
+x_tr = np.empty([0,nchans,epoch_size])
+y_tr = np.empty([0,nlabels])
+x_ts = np.empty([0,nchans,epoch_size])
+y_ts = np.empty([0,nlabels])
 
 for isubj_count, (subj, isubj) in enumerate(subj_names_ids.items()):
 	for irec in range(8):
@@ -28,17 +30,18 @@ for isubj_count, (subj, isubj) in enumerate(subj_names_ids.items()):
 		all_trigger_events = antbeat_utils.data_utils.load_trigger_events(data_dir,subj,isubj,irec+1,phase_trial_type_trigger_dict)
 		epochs = antbeat_utils.data_utils.epoch_raw_with_ssp(all_trigger_events,phase_trial_type_trigger_dict, raw,subj,irec, tmin=-0.1, tmax=0.5)
 		labels = antbeat_utils.data_utils.generate_epoch_label_matrix(all_trigger_events,phase_trigger_dict,trial_type_trigger_dict,isubj_count)
-		curr_epochs = epochs.get_data()[:,:64,::4] # downsample and keep only eeg channels
-		all_subj_epochs = np.concatenate(
-			(all_subj_epochs,curr_epochs),axis=0)
-		all_subj_labels = np.concatenate(
-			(all_subj_labels,labels),axis=0)
-
-x_tr,y_tr,x_ts,y_ts = antbeat_utils.data_utils.split_data_for_cross_validation(all_subj_epochs, all_subj_labels)
+		epochs = epochs.resample(125)
+		curr_epochs = epochs.get_data()[:,:64,:] # downsample and keep only eeg channels
+		x_tr = np.concatenate(
+			(x_tr,curr_epochs[:-60]),axis=0)
+		y_tr = np.concatenate(
+			(y_tr,labels[:-60]),axis=0)
+		x_ts = np.concatenate(
+			(x_ts,curr_epochs[-60:]),axis=0)
+		y_ts = np.concatenate(
+			(y_ts,labels[-60:]),axis=0)
 
 np.save('tr_data/x_tr',x_tr)
 np.save('tr_data/y_tr',y_tr)
 np.save('ts_data/x_ts',x_ts)
 np.save('ts_data/y_ts',y_ts)
-
-		
